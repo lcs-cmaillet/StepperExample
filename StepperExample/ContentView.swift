072f5f1efc0f3564f7ceb5c359e0a6c27dc26fac
Collin Maillet <cmaillet@lcs.on.ca>
2025-01-13 09:27:13 -0500

    Added a stored property to hold the base of the power selected by the user.

--- StepperExample/ContentView.swift
+++ StepperExample/ContentView.swift
@@ -2,23 +2,42 @@
 //  ContentView.swift
 //  StepperExample
 //
-//  Created by Collin Maillet on 2025-01-13.
-//
-
+ 
 import SwiftUI
-
+ 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var base: Int = 1
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            HStack(alignment: .top) {
+ 
+                Text("\(base)")
+                    .font(.system(size: 96))
+ 
+                Text("2")
+                    .font(.system(size: 44))
+ 
+                Text("=")
+                    .font(.system(size: 96))
+ 
+                Text("25")
+                    .font(.system(size: 96))
+            }
+                        
+            Spacer()
         }
         .padding()
     }
 }
-
+ 
 #Preview {
     ContentView()
 }
+ 
